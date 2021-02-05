9ce19466435778bd6628bf647cfe023d2cb5ae23
razumkov <razumkov@e1.ru>
2021-02-05 14:11:56 +0500

    add custom type and custom fields for this type

--- home/2021_wsr_4/wp-content/themes/wordsmith/functions.php
+++ home/2021_wsr_4/wp-content/themes/wordsmith/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function includeCss() {
     wp_enqueue_style('base_css', get_stylesheet_directory_uri() . '/css/base.css');
     wp_enqueue_style('vendor_css', get_stylesheet_directory_uri() . '/css/vendor.css');
@@ -39,10 +40,67 @@ function customize_register(WP_Customize_Manager $customizer) {
     );
 }
 
+function add_animal_type_post(){
+    register_post_type('animal', array(
+        'labels'             => array(
+            'name'               => 'Животные', // Основное название типа записи
+            'singular_name'      => 'Животное', // отдельное название записи типа Book
+            'add_new'            => 'Добавить нового',
+            'add_new_item'       => 'Добавить нового животного',
+            'edit_item'          => 'Редактировать животное',
+            'new_item'           => 'Новое животное',
+            'view_item'          => 'Посмотреть животное',
+            'search_items'       => 'Найти животное',
+            'not_found'          => 'Животных не найдено',
+            'menu_name'          => 'Животные'
+
+        ),
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => true,
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array('title', 'thumbnail')
+    ) );
+}
+
+
+function animal_add_fields() {
+    add_meta_box( 'extra_fields', 'Дополнительные поля', 'extra_fields_box_func', 'animal', 'normal', 'high');
+}
+
+// код блока
+function extra_fields_box_func( $post ){
+    ?>
+    <p><label><input type="text" name="extra[title]" value="<?php echo get_post_meta($post->ID, 'title', 1); ?>" style="width:50%" /> ? заголовок страницы (title)</label></p>
+
+    <p>Описание статьи (description):
+        <textarea type="text" name="extra[description]" style="width:100%;height:50px;"><?php echo get_post_meta($post->ID, 'description', 1); ?></textarea>
+    </p>
+
+    <p><select name="extra[select]">
+            <?php $sel_v = get_post_meta($post->ID, 'select', 1); ?>
+            <option value="0">----</option>
+            <option value="1" <?php selected( $sel_v, '1' )?> >Выбери меня</option>
+            <option value="2" <?php selected( $sel_v, '2' )?> >Нет, меня</option>
+            <option value="3" <?php selected( $sel_v, '3' )?> >Лучше меня</option>
+        </select> ? выбор за вами</p>
+
+    <input type="hidden" name="extra_fields_nonce" value="<?php echo wp_create_nonce(__FILE__); ?>" />
+    <?php
+}
 
 
 add_action('customize_register', 'customize_register');
-
+add_action('add_meta_boxes', 'animal_add_fields', 1);
 add_action('wp_enqueue_scripts', 'includeCss');
 add_action('wp_enqueue_scripts', 'includeJs');
 add_action('after_setup_theme', 'theme_register_nav_menu');
+add_action('init', 'add_animal_type_post');
+
+add_theme_support( 'post-thumbnails' );
--- home/2021_wsr_4/wp-content/themes/wordsmith/index.php
+++ home/2021_wsr_4/wp-content/themes/wordsmith/index.php
@@ -18,5 +18,67 @@
     </div>
 </section>
 
+<section class="s-content">
+
+    <div class="row narrow">
+        <div class="col-full s-content__header aos-init aos-animate" data-aos="fade-up">
+            <h1 class="display-1 display-1--with-line-sep">Наши питомцы</h1>
+        </div>
+    </div>
+
+    <div class="row entries-wrap wide">
+        <div class="entries">
+
+            <?php
+
+            $pages = get_pages([
+                'meta_key' => '_wp_page_template',
+                'meta_value' => 'page_animal.php'
+            ]);
+            $limit = 6;
+            $pagesFiltered = [];
+            shuffle($pages);
+
+            foreach ($pages as $page) {
+                if ($limit > 0) {
+                    $limit--;
+                    $pagesFiltered[] = $page;
+                }
+            }
+
+            /** @var WP_Post $page */
+            foreach($pagesFiltered as $page): ?>
+            <?php
+                $filteredContent = str_replace('<br>', PHP_EOL, $page->post_content);
+                $pageArialArray = [];
+                preg_match("/Ареал.*\n/iu", $filteredContent, $pageArialArray);
+                $arial = $pageArialArray[0];
+            ?>
+            <article class="col-block">
+
+                <div class="item-entry aos-init aos-animate" data-aos="zoom-in">
+                    <div class="item-entry__thumb">
+                        <a href="<?=$page->guid?>" class="item-entry__thumb-link">
+                            <img src="<?= get_the_post_thumbnail_url($page->ID); ?>" alt="">
+                        </a>
+                    </div>
+
+                    <div class="item-entry__text">
+                        <h1 class="item-entry__title"><a href="<?=$page->guid?>"><?= $page->post_title; ?></a></h1>
+
+                        <div class="item-entry__text">
+                            <?=$arial?>
+                        </div>
+                    </div>
+                </div> <!-- item-entry -->
+
+            </article> <!-- end article -->
+
+            <?php endforeach; ?>
+
+        </div> <!-- end entries -->
+    </div> <!-- end entries-wrap -->
+</section>
+
 <?php get_footer(); ?>
 
